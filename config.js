6a2ca0da7b2e24efb4c97e68a88df36ec3854044
SERGIO <sergio@comovas.es>
2022-08-03 13:15:35 +0200

    Cambios agosto 2022

--- config.js
+++ config.js
@@ -9,33 +9,6 @@ const config = {
         templateConfig: {},
         sourceDir: 'source',
         buildDir: 'build',
-<<<<<<< HEAD
-        summaryLength: 250,
-        port: 4965,
-        pagination: true,
-        postPerPage: 8
-    },
-    production: {
-        name: 'COMOVAS |『CØMO』『VAS』.ES | ',
-        description: '¿Cómo vas? Nosotros vamos tirando con NWO, GAMING, TECH, SALSEO, PODCASTS y mucho más...',
-        twitterHandle: '@sergiocomovas',
-        baseUrl: 'http://comovas.es',
-        sourcesExt: 'markdown',
-        templateEngine: 'hbs',
-        templateConfig: {},
-        sourceDir: 'source',
-        buildDir: 'build',
-        summaryLength: 250,
-        port: 4965,
-        pagination: true,
-        postPerPage: 8,
-        postBuild: [
-            {
-                name: 'zip',
-                options: {}
-            }
-        ]
-=======
         summaryLength: 250,
         port: 4965,
         pagination: true,
@@ -53,7 +26,6 @@ const config = {
         port: 4965,
         pagination: true,
         postPerPage: 8
->>>>>>> 7011b9e312a236ec785c322ddc4f16dceedb5e0f
     }
 };
 
